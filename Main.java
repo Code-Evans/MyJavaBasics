a3c4d88ffd8d5ceca0cba0a9e49599a6b7d8efe8
Code-Evans <adjsevangelista@gmail.com>
2025-10-15 01:10:19 +0800

    updated Data Types

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    
+    public static void main(String[] args) { 
+
+        //Data Types in Java
+        
+        /** 
+         
+         Primitive Data Types
+         
+         * These are the basic data types built into Java. 
+         * They store actual values directly in memory.
+         * They are faster  and use less memory.
+         * They cannot be null.
+         
+         **/
+
+        
+        byte numByte = 127; // 8 bits, -128 to 127
+        short numShort = 32_768; // 16 bits, -32,768 to 32,767
+        int numInt = 2_147_483_647; // 32 bits, -2,147,483,648 to 2,147,483,647
+        Long numLong = 9_223_372_036_854_775_807L; // 64 bits, -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        float numFloat = 3.141592F; //32 bits, 6 to 7 digits Decimal Values
+        double numDouble = 3.141592653589793; // 64 bits, 15 to 16 digits Decimal Values
+        char letter; // Only one letter character value, a to z or A to Z
+        boolean bool = true; // Only True or False value
+
+
+    }
+}
